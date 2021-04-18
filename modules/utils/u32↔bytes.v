3ed184b151e073caa373fa05357dfc87a577945a
Ivan Vatlin <16368461+jeffangelion@users.noreply.github.com>
2021-04-18 15:23:45 +0300

    Improving u32↔bytes.v
    + no math import for pow
    + now using shifts
    + removing u32_to_bytes() since there is no need for now

--- modules/utils/u32↔bytes.v
+++ modules/utils/u32↔bytes.v
@@ -1,15 +1,9 @@
 module utils
 
-import math
-
 pub fn bytes_to_u32(bytes []byte) u32 {
 	mut temp := u32(0)
 	for i in 0..bytes.len { 
-		temp += u32(bytes[i]) * u32(math.pow(16, i * 2))
+		temp += bytes[i] << ((i) * 8)
 	}
 	return temp
-}
-
-pub fn u32_to_bytes(nb u32) []byte {
-	return [byte((nb >> 24) & 0xFF), byte((nb >> 16) & 0xFF), byte((nb >> 8) & 0xFF), byte(nb & 0xFF)]
 }
